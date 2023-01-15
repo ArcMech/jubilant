4dd8076d439771eac25bcda60e497ef13b9a2dcf
Arkadiusz Koziarz <arek.krz@gmail.com>
2023-01-15 20:42:20 +0100

    [api/feat] Add puppeteer, generate pdf from dynamic html

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import ejs from "ejs";
+import puppeteer from "puppeteer";
 import { getMe } from "./services";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -11,8 +13,47 @@ app.use(bodyParser.json());
 app.use(cors());
 app.set("view engine", "ejs");
 
+// @todo Move it to helpers/services
+const makeIt = async ({
+  name,
+  projects,
+}: {
+  name: string;
+  projects: { name: string; tasks: { name: string }[] }[];
+}) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  let dynamicPage = await ejs.renderFile(
+    path.join(__dirname + "/views/protocole.ejs"),
+    {
+      name,
+      projects,
+    }
+  );
+
+  await page.setContent(dynamicPage, {
+    waitUntil: "domcontentloaded",
+  });
+
+  // or a .pdf file
+  await page.pdf({
+    format: "A4",
+    path: `${__dirname}/protokol.pdf`,
+  });
+
+  // close the browser
+  await browser.close();
+};
+
 app.get("/", (req, res) => {
-  res.render(path.join(__dirname + "/views/protocole"), {
+  res.send("Hello Jubilate");
+});
+
+app.get("/protocole", async (req, res) => {
+  await makeIt({
     name: "Jane Doe",
     projects: [
       {
